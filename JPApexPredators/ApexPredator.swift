5d91dcdb0cc655c01d3105a4c3e996a8f65386a0
Alex Bibik <alexandr.bibik@gmail.com>
2026-05-11 12:55:46 +0300

    filtering

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,67 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Олександр Бібік on 04.05.2026.
+//
+
+import Foundation
+import SwiftUI
+
+struct ApexPredator: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let type: APType
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    struct MovieScene: Decodable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+}
+
+enum APType: String, Decodable, CaseIterable, Identifiable {
+    case all
+    case land
+    case air
+    case sea
+    
+    var id: APType {
+        self
+    }
+    
+    var backgroud: Color {
+        switch self {
+        case .land:
+                .brown
+        case .air:
+                .teal
+        case .sea:
+                .blue
+        case .all:
+                .black
+        }
+    }
+    
+    var icon: String {
+        switch self {
+        case .all:
+            "square.stack.3d.up.fill"
+        case .land:
+            "leaf.fill"
+        case .air:
+            "wind"
+        case .sea:
+            "drop.fill"
+        }
+    }
+}
---
+++ JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by Олександр Бібік on 11.05.2026.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
